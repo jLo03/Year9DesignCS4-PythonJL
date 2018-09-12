2a79e5d1ac15ef4aab4f5a77bdfc35dca3913d34
Justin Lo <justin.lo@Y22-Design-JustinLo2.local>
2018-09-12 13:41:02 -0400

    Takin Input

---
+++ TakingInput.py
@@ -0,0 +1,14 @@
+#Input
+#Process
+#Output
+
+question=list(input)
+for rep in range(3):
+	for i in range(len(question)):
+		if question[i]=='=':
+			value=question[i+1]:
+	for i in range(len(quesiton)):
+		
+
+
+
