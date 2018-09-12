9a10d4d25d11b33731588d1e5b1471ae802ca8ed
Justin Lo <justin.lo@Y22-Design-JustinLo2.local>
2018-09-12 14:02:28 -0400

    g

--- TakingInput.py
+++ TakingInput.py
@@ -2,12 +2,26 @@
 #Process
 #Output
 
-question=list(input)
-for rep in range(3):
-	for i in range(len(question)):
-		if question[i]=='=':
-			value=question[i+1]:
-	for i in range(len(quesiton)):
+
+import os
+
+os.system("cd desktop")
+os.system("cd CS")
+os.system("cd Programming")
+os.system("cd GitRepoYear9")
+os.system("cd Year9DesignCS4-PythonJL")
+
+text=input('What is your comment for commit: ')
+
+os.system("git status")
+os.system("git add .")
+
+sentence = str('git commit -m "{0}"'.format(text))
+os.system(sentence)
+
+os.system("git push")
+
+
 		
 
 
